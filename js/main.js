1b7eecb9413820b0a5cf3ec516765ba9d4e40290
grit96 <geraintwhite@gmail.com>
2013-06-08 12:57:15 +0100

    Nice js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
-$.fn.teletype = function(opts){
-	var $this = this,
-		defaults = {
-			animDelay: 50
-		},
-		settings = $.extend(defaults, opts);
-
-	$.each(settings.text.split(''), function(i, letter){
+function teletype(el, animDelay, text, callback){
+	$.each(text.split(''), function(i, letter){
 		setTimeout(function(){
-			$this.html($this.html() + letter);
-		}, settings.animDelay * i);
+			el.append(letter);
+		}, animDelay * i);
 	});
+	setTimeout(function(){
+		callback();
+	}, text.split('').length*animDelay);
 };
 
 $(document).ready(function(){
 	$('#intro, #sites').css('display', 'none');
-	$('body').prepend("<section class=\"text\" id=\"start\"></section>");
-	$('#start').teletype({
-		animDelay: 100,
-		text: '$ pi@raspberrypi - '
-	});
-	setTimeout(function(){
-		$('#intro').fadeIn('slow', function() {
-			$('#sites').fadeIn('slow');
+	$('body').prepend("<section class=\"text bash\"></section>");
+	teletype($('.bash:first-child'), 100, '$ pi@raspberrypi - ', function() {
+		$('.bash:first-child').append('<br><br>');
+		teletype($('.bash:first-child'), 100, '$ pi@raspberrypi - cat intro.txt', function() {
+			setTimeout(function(){
+				$('#intro').fadeIn('slow', function() {
+					$('#intro').after("<section class=\"text bash\"></section>");
+					teletype($('.bash:nth-child(3)'), 100, '$ pi@raspberrypi - cat sites.txt', function() {
+						setTimeout(function(){
+							$('#sites').fadeIn('slow');
+						}, 200);
+					});
+				});
+			}, 200);
 		});
-	}, 2000);
+	});
 });
