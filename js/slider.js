ccef0e39e1d417a5c009f6d5a2642924227e5176
Geraint White <geraint@android.net>
2014-03-04 22:13:25 +0000

    Fixed prevent onclick event

--- js/slider.js
+++ js/slider.js
@@ -53,14 +53,14 @@ function update() {
 
 update();
 
-addEvent(prev, 'click', function () {
+addEvent(prev, 'click', function (event) {
   if (pointer > 0) {
     pointer--;
     update();
   }
   event.preventDefault ? event.preventDefault() : event.returnValue = false;
 });
-addEvent(next, 'click', function () {
+addEvent(next, 'click', function (event) {
   if (pointer < sites.length - 3) {
     pointer++;
     update();
