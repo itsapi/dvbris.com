3c629dee712f88052aea460f1104c3614e6f0a1b
Oliver Faircliff <olls@ovi.com>
2014-03-11 20:22:05 +0000

    Fixed htmlmin and continual expansion of css and js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,10 @@ module.exports = function(grunt) {
     uglify: {
       combine: {
         options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n\n'
         },
         files: {
-          'js/main.min.js': ['js/*.js']
+          'js/dist/main.min.js': ['js/src/*.js']
         }
       }
     },
@@ -19,7 +19,25 @@ module.exports = function(grunt) {
           banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
         },
         files: {
-          'css/main.min.css': ['css/*.css']
+          'css/dist/main.min.css': ['css/src/*.css']
+        }
+      }
+    },
+    htmlmin: {
+      dev: {
+        options: {
+          banner: '<!--! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> -->\n',
+          removeComments: true,
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeAttributeQuotes: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true
+        },
+        files: {
+          'index.html': 'index.dev.html'
         }
       }
     }
@@ -27,9 +45,9 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  // grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'cssmin']);
+  grunt.registerTask('default', ['uglify', 'cssmin', 'htmlmin']);
 
 };
