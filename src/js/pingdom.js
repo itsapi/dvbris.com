9cfe42f2987699d08396b565ffdc42080777f505
Geraint White <geraint@android.net>
2014-03-15 23:12:47 +0000

    Added Pingdom script

---
+++ src/js/pingdom.js
@@ -0,0 +1,9 @@
+var _prum = [['id', '52b85c12abe53d3f32000000'],
+             ['mark', 'firstbyte', (new Date()).getTime()]];
+(function() {
+    var s = document.getElementsByTagName('script')[0]
+      , p = document.createElement('script');
+    p.async = 'async';
+    p.src = '//rum-static.pingdom.net/prum.min.js';
+    s.parentNode.insertBefore(p, s);
+})();
