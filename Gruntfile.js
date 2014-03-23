4ee5ecf3d295fe9a1c22e1fdfdc4d8d89f7bc9da
Geraint White <geraint@android.net>
2014-03-23 21:26:40 +0000

    Loaded tasks in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,7 +84,9 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
